233102f41972106a55be3a00e3654ec1dbe4e709
raihanabanu <raihanabanu.s@gmail.com>
2016-08-07 23:18:51 -0700

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,12 @@
+# Factra
+int fact=1;
+int i;
+int num=Integer.parseInt(args[0]);
+if(num==0){
+fact=fact+1;
+System.out.println(fact);
+}
+for(int i=1;i<num;i++){
+fact=fact*i;
+system.out.println(fact);
+}
